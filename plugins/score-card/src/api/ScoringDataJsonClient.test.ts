29c568645edcfa4a79ffc21d4b489b3d6e2a5a24
Tyagaraj, Anand <69570136+anand-tyagaraj@users.noreply.github.com>
2023-11-21 17:54:39 +0530

    fixed an issue

--- plugins/score-card/src/api/ScoringDataJsonClient.test.ts
+++ plugins/score-card/src/api/ScoringDataJsonClient.test.ts
@@ -19,11 +19,26 @@ import { CatalogApi } from '@backstage/plugin-catalog-react';
 import { Entity } from '@backstage/catalog-model';
 import { ScoringDataJsonClient } from './ScoringDataJsonClient';
 
-
 import {
   GetEntitiesRequest,
   GetEntitiesResponse,
 } from '@backstage/catalog-client';
+
+const mockOctokit = {
+  request: jest.fn(),
+};
+
+jest.mock('@octokit/rest', () => ({
+  Octokit: class {
+    constructor() {
+      return mockOctokit;
+    }
+  },
+}));
+
+const mockAuth = {
+  getAccessToken: jest.fn(),
+};
 
 // Catalog items used as mock
 const items = [
@@ -32,6 +47,10 @@ const items = [
     kind: 'API',
     metadata: {
       name: 'Api 1',
+      annotations: {
+        'scorecard/jsonDataUrl': 'dummy',
+        'github.com/project-slug': 'org/repo',
+      },
     },
     spec: {
       type: 'openapi',
@@ -69,43 +88,57 @@ const getEntitiesMock = (
       ? request?.filter[0].kind ?? []
       : [];
   return Promise.resolve({
-    items: filterKinds.length? items.filter(item => filterKinds.find(k => k === item.kind)) : items,
+    items: filterKinds.length
+      ? items.filter(item => filterKinds.find(k => k === item.kind))
+      : items,
   } as GetEntitiesResponse);
+};
+const mockedScoreEnt1 = {
+  entityRef: {
+    kind: 'api',
+    name: 'Api 1',
+  },
+  scorePercent: 75,
+  scoringReviewDate: '2022-01-01T08:00:00Z',
+  scoringReviewer: { kind: 'User', name: 'Reviewer 1', namespace: 'default' },
+  areaScores: [],
 };
 
 describe('ScoringDataJsonClient-getAllScores', () => {
   beforeEach(() => {
-    jest.spyOn(global, "fetch").mockImplementation(
-      jest.fn(() => Promise.resolve({
+    jest.spyOn(global, 'fetch').mockImplementation(
+      jest.fn(() =>
+        Promise.resolve({
           status: 200,
-          json: () => new Promise((resolve) => {
-            const sampleData = [
-              {
-                "entityRef": {
-                  "kind": "api",
-                  "name": "Api 1"
+          json: () =>
+            new Promise(resolve => {
+              const sampleData = [
+                {
+                  entityRef: {
+                    kind: 'api',
+                    name: 'Api 1',
+                  },
+                  scorePercent: 75,
+                  scoringReviewDate: '2022-01-01T08:00:00Z',
+                  scoringReviewer: 'Reviewer 1',
+                  areaScores: [],
                 },
-                "scorePercent": 75,
-                "scoringReviewDate": "2022-01-01T08:00:00Z",
-                "scoringReviewer": "Reviewer 1",
-                "areaScores": []
-              },
-              {
-                "entityRef": {
-                  "kind": "system",
-                  "name": "System 1"
+                {
+                  entityRef: {
+                    kind: 'system',
+                    name: 'System 1',
+                  },
+                  scorePercent: 80,
+                  scoringReviewDate: '2022-01-01T08:00:00Z',
+                  scoringReviewer: 'Reviewer 2',
+                  areaScores: [],
                 },
-                "scorePercent": 80,
-                "scoringReviewDate": "2022-01-01T08:00:00Z",
-                "scoringReviewer": "Reviewer 2",
-                "areaScores": []
-              },
-            ]
-            resolve(sampleData);
-          }),
+              ];
+              resolve(sampleData);
+            }),
         }),
-      ) as jest.Mock
-    )
+      ) as jest.Mock,
+    );
   });
 
   afterEach(() => {
@@ -113,7 +146,6 @@ describe('ScoringDataJsonClient-getAllScores', () => {
   });
 
   it('should get all scores', async () => {
-
     const catalogApi: jest.Mocked<CatalogApi> = {
       getEntities: jest.fn(),
     } as any;
@@ -127,31 +159,31 @@ describe('ScoringDataJsonClient-getAllScores', () => {
 
     const expected = [
       {
-        "areaScores": [],
-        "entityRef": {"kind": "api", "name": "Api 1"},
-        "owner": {"kind": "group", "name": "team1", "namespace": "default"},
-        "reviewDate": new Date('2022-01-01T08:00:00.000Z'),
-        "reviewer": {"kind": "User", "name": "Reviewer 1", "namespace": "default"},
-        "scorePercent": 75,
-        "scoringReviewDate": "2022-01-01T08:00:00Z",
-        "scoringReviewer": "Reviewer 1"
+        areaScores: [],
+        entityRef: { kind: 'api', name: 'Api 1' },
+        owner: { kind: 'group', name: 'team1', namespace: 'default' },
+        reviewDate: new Date('2022-01-01T08:00:00.000Z'),
+        reviewer: { kind: 'User', name: 'Reviewer 1', namespace: 'default' },
+        scorePercent: 75,
+        scoringReviewDate: '2022-01-01T08:00:00Z',
+        scoringReviewer: 'Reviewer 1',
       },
       {
-        "areaScores": [],
-        "entityRef": {"kind": "system", "name": "System 1"},
-        "owner": {"kind": "group", "name": "team2", "namespace": "default"},
-        "reviewDate": new Date('2022-01-01T08:00:00.000Z'),
-        "reviewer": {"kind": "User", "name": "Reviewer 2", "namespace": "default"},
-        "scorePercent": 80,
-        "scoringReviewDate": "2022-01-01T08:00:00Z",
-        "scoringReviewer": "Reviewer 2"
-      }
+        areaScores: [],
+        entityRef: { kind: 'system', name: 'System 1' },
+        owner: { kind: 'group', name: 'team2', namespace: 'default' },
+        reviewDate: new Date('2022-01-01T08:00:00.000Z'),
+        reviewer: { kind: 'User', name: 'Reviewer 2', namespace: 'default' },
+        scorePercent: 80,
+        scoringReviewDate: '2022-01-01T08:00:00Z',
+        scoringReviewer: 'Reviewer 2',
+      },
     ];
 
     const api = new ScoringDataJsonClient({
       configApi: mockConfig,
       fetchApi: mockFetch,
-      catalogApi: catalogApi
+      catalogApi: catalogApi,
     });
 
     const entities = await api.getAllScores();
@@ -159,7 +191,6 @@ describe('ScoringDataJsonClient-getAllScores', () => {
   });
 
   it('should filter entities by kind', async () => {
-
     const catalogApi: jest.Mocked<CatalogApi> = {
       getEntities: jest.fn(),
     } as any;
@@ -173,24 +204,241 @@ describe('ScoringDataJsonClient-getAllScores', () => {
 
     const expected = [
       {
-        "areaScores": [],
-        "entityRef": {"kind": "api", "name": "Api 1"},
-        "owner": {"kind": "group", "name": "team1", "namespace": "default"},
-        "reviewDate": new Date('2022-01-01T08:00:00.000Z'),
-        "reviewer": {"kind": "User", "name": "Reviewer 1", "namespace": "default"},
-        "scorePercent": 75,
-        "scoringReviewDate": "2022-01-01T08:00:00Z",
-        "scoringReviewer": "Reviewer 1"
-      }
+        areaScores: [],
+        entityRef: { kind: 'api', name: 'Api 1' },
+        owner: { kind: 'group', name: 'team1', namespace: 'default' },
+        reviewDate: new Date('2022-01-01T08:00:00.000Z'),
+        reviewer: { kind: 'User', name: 'Reviewer 1', namespace: 'default' },
+        scorePercent: 75,
+        scoringReviewDate: '2022-01-01T08:00:00Z',
+        scoringReviewer: 'Reviewer 1',
+      },
     ];
 
     const api = new ScoringDataJsonClient({
       configApi: mockConfig,
       fetchApi: mockFetch,
-      catalogApi: catalogApi
+      catalogApi: catalogApi,
     });
 
     const entities = await api.getAllScores(['api']);
     expect(entities).toEqual(expected);
   });
+});
+
+describe('ScoringDataJsonClient-getAllScores-annotation', () => {
+  beforeEach(() => {
+    const mockedAllJson = [
+      mockedScoreEnt1,
+      {
+        entityRef: {
+          kind: 'system',
+          name: 'System 1',
+        },
+        scorePercent: 80,
+        scoringReviewDate: '2022-01-01T08:00:00Z',
+        scoringReviewer: 'Reviewer 2',
+        areaScores: [],
+      },
+    ];
+    mockOctokit.request.mockResolvedValue({
+      status: 200,
+      data: JSON.stringify(mockedAllJson),
+    });
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should get all scores', async () => {
+    const catalogApi: jest.Mocked<CatalogApi> = {
+      getEntities: jest.fn(),
+    } as any;
+
+    catalogApi.getEntities.mockImplementation(getEntitiesMock);
+
+    const mockConfig = new MockConfigApi({
+      app: { baseUrl: 'https://example.com' },
+    });
+
+    const expected = [
+      {
+        areaScores: [],
+        entityRef: { kind: 'api', name: 'Api 1' },
+        owner: { kind: 'group', name: 'team1', namespace: 'default' },
+        reviewDate: new Date('2022-01-01T08:00:00.000Z'),
+        reviewer: { kind: 'User', name: 'Reviewer 1', namespace: 'default' },
+        scorePercent: 75,
+        scoringReviewDate: '2022-01-01T08:00:00Z',
+        scoringReviewer: {
+          kind: 'User',
+          name: 'Reviewer 1',
+          namespace: 'default',
+        },
+      },
+      {
+        areaScores: [],
+        entityRef: { kind: 'system', name: 'System 1' },
+        owner: { kind: 'group', name: 'team2', namespace: 'default' },
+        reviewDate: new Date('2022-01-01T08:00:00.000Z'),
+        reviewer: { kind: 'User', name: 'Reviewer 2', namespace: 'default' },
+        scorePercent: 80,
+        scoringReviewDate: '2022-01-01T08:00:00Z',
+        scoringReviewer: 'Reviewer 2',
+      },
+    ];
+
+    const mockFetch = new MockFetchApi();
+
+    const api = new ScoringDataJsonClient({
+      configApi: mockConfig,
+      fetchApi: mockFetch,
+      catalogApi: catalogApi,
+    });
+
+    const entities = await api.getAllScores(undefined, items[0], mockAuth);
+    expect(entities).toEqual(expected);
+  });
+
+  
+
+  it('should return error on getAllScore()', async () => {
+    const catalogApi: jest.Mocked<CatalogApi> = {
+      getEntities: jest.fn(),
+    } as any;
+
+    catalogApi.getEntities.mockImplementation(getEntitiesMock);
+
+    const mockConfig = new MockConfigApi({
+      app: { baseUrl: 'https://example.com' },
+    });
+
+    const mockFetch = new MockFetchApi();
+
+    const api = new ScoringDataJsonClient({
+      configApi: mockConfig,
+      fetchApi: mockFetch,
+      catalogApi: catalogApi,
+    });
+
+    mockOctokit.request.mockResolvedValue({
+      status: 500,
+    });
+
+    await expect(
+      api.getAllScores(undefined, items[0], mockAuth),
+    ).rejects.toThrow('error from server (code 500)');
+  });
+
+  it('should filter entities by kind', async () => {
+    const catalogApi: jest.Mocked<CatalogApi> = {
+      getEntities: jest.fn(),
+    } as any;
+
+    catalogApi.getEntities.mockImplementation(getEntitiesMock);
+
+    const mockConfig = new MockConfigApi({
+      app: { baseUrl: 'https://example.com' },
+    });
+
+    const expected = [
+      {
+        areaScores: [],
+        entityRef: { kind: 'api', name: 'Api 1' },
+        owner: { kind: 'group', name: 'team1', namespace: 'default' },
+        reviewDate: new Date('2022-01-01T08:00:00.000Z'),
+        reviewer: { kind: 'User', name: 'Reviewer 1', namespace: 'default' },
+        scorePercent: 75,
+        scoringReviewDate: '2022-01-01T08:00:00Z',
+        scoringReviewer: {
+          kind: 'User',
+          name: 'Reviewer 1',
+          namespace: 'default',
+        },
+      },
+    ];
+
+    const mockFetch = new MockFetchApi();
+
+    const api = new ScoringDataJsonClient({
+      configApi: mockConfig,
+      fetchApi: mockFetch,
+      catalogApi: catalogApi,
+    });
+
+    const entities = await api.getAllScores(['api'], items[0], mockAuth);
+    expect(entities).toEqual(expected);
+  });
+
+  it('should get component scores', async () => {
+    const catalogApi: jest.Mocked<CatalogApi> = {
+      getEntities: jest.fn(),
+    } as any;
+
+    catalogApi.getEntities.mockImplementation(getEntitiesMock);
+
+    const mockConfig = new MockConfigApi({
+      app: { baseUrl: 'https://example.com' },
+    });
+    mockOctokit.request.mockResolvedValue({
+      status: 200,
+      data: JSON.stringify(mockedScoreEnt1),
+    });
+
+    const expected = {
+      areaScores: [],
+      entityRef: { kind: 'api', name: 'Api 1' },
+      reviewDate: new Date('2022-01-01T08:00:00.000Z'),
+      reviewer: { kind: 'User', name: 'Reviewer 1', namespace: 'default' },
+      scorePercent: 75,
+      scoringReviewDate: '2022-01-01T08:00:00Z',
+      scoringReviewer: {
+        kind: 'User',
+        name: 'Reviewer 1',
+        namespace: 'default',
+      },
+    };
+    const mockFetch = new MockFetchApi();
+
+    const api = new ScoringDataJsonClient({
+      configApi: mockConfig,
+      fetchApi: mockFetch,
+      catalogApi: catalogApi,
+    });
+
+    const entities = await api.getScore(items[0], mockAuth);
+
+    expect(entities).toEqual(expected);
+  });
+
+  it('should return error on getScore', async () => {
+    const catalogApi: jest.Mocked<CatalogApi> = {
+      getEntities: jest.fn(),
+    } as any;
+
+    catalogApi.getEntities.mockImplementation(getEntitiesMock);
+
+    const mockConfig = new MockConfigApi({
+      app: { baseUrl: 'https://example.com' },
+    });
+
+    const mockFetch = new MockFetchApi();
+
+    const api = new ScoringDataJsonClient({
+      configApi: mockConfig,
+      fetchApi: mockFetch,
+      catalogApi: catalogApi,
+    });
+
+    mockOctokit.request.mockResolvedValue({
+      status: 500,
+    });
+
+    await expect(api.getScore(items[0], mockAuth)).rejects.toThrow(
+      'error from server (code 500)',
+    );
+  });
+
+  
 });
